12f67dac502113465ff4386f66b295c6437cb6fe
yuexiarenjing <yuexiarenjing@gmail.com>
2016-07-30 13:36:00 +0800

    add two maven project

---
+++ testmaven-demo/src/main/java/edu/tj/testmaven/testmaven_demo/App.java
@@ -0,0 +1,13 @@
+package edu.tj.testmaven.testmaven_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
